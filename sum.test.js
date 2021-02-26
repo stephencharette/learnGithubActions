19fffc39107e1184227ce77518b0c7d987675394
Jeff Schuman <jschuman@solutionstreet.com>
2021-02-26 10:47:41 -0500

    add some tests

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,8 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('sum defaults inputs to 0', () => {
+  expect(sum(1)).toBe(1);
+  expect(sum()).toBe(0);
+});
